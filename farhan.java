3c60751b0c99035286c72d9fdc98bde829c5e93c
imrankhan8686 <imrankhan.arbz@gmail.com>
2025-07-17 13:13:17 +0530

    added two more logs

--- farhan.java
+++ farhan.java
@@ -4,5 +4,8 @@ public class Farhan
     {
       System.oiut.println("my school name is vips international school");
       System.out.println("my standeds is PP1"):
+        System.out.println("My frnd name is Izhan");
+      System.out.println("My class Teacher name is Saniya Mam");
+      
       }
   }
