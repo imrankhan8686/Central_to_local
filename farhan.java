d2039c8f9ef35bcd908e0314934bb5708d70d8d5
imrankhan8686 <imrankhan.arbz@gmail.com>
2025-07-17 12:57:20 +0530

    aded one more file farhan

---
+++ farhan.java
@@ -0,0 +1,8 @@
+public class Farhan
+  {
+    public static void main (String [] args)
+    {
+      System.oiut.println("my school name is vips international school");
+      System.out.println("my standeds is PP1"):
+      }
+  }
