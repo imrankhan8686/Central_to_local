50a1d7d38c08372c9a742c15dbc6378fbc7e2b67
imrankhan8686 <imrankhan.arbz@gmail.com>
2025-07-17 13:23:56 +0530

    Delete arbz.java

--- arbz.java
+++
@@ -1,8 +0,0 @@
-public class 
-{
-public static void main (String [ ] args)
-{
-System.out.println("Hello Arbz im doing central to local");
-System.out.println("so thats why i created a file in central");
-}
-}
